8d1fd84aff148b919a516f399c39102519f34040
grantsmith <grantsmith@13f79535-47bb-0310-9956-ffa450edef68>
2005-07-08 16:11:58 +0000

    MYFACES-313: Patch to allow UISaveState to use saveAttachedState to allow the user implement StateHolder for values. Submitted by Mathias Broekelmann.
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@209834 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/myfaces/custom/savestate/UISaveState.java
+++ src/java/org/apache/myfaces/custom/savestate/UISaveState.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,7 +32,7 @@ public class UISaveState
     {
         Object values[] = new Object[2];
         values[0] = super.saveState(context);
-        values[1] = getValue();
+        values[1] = saveAttachedState(context, getValue());
         return ((Object) (values));
     }
 
@@ -40,7 +40,7 @@ public class UISaveState
     {
         Object values[] = (Object[])state;
         super.restoreState(context, values[0]);
-        Object value = values[1];
+        Object value = restoreAttachedState(context, values[1]);
         ValueBinding vb = getValueBinding("value");
         if (vb != null)
         {
