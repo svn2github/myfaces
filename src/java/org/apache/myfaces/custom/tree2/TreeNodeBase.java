2a828cd395b60616e281dbcbbadab9458860bb58
schof <schof@13f79535-47bb-0310-9956-ffa450edef68>
2005-08-04 03:03:32 +0000

    Additional changes related to MYFACES-350
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@227295 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/myfaces/custom/tree2/TreeNodeBase.java
+++ src/java/org/apache/myfaces/custom/tree2/TreeNodeBase.java
@@ -16,21 +16,23 @@
 
 package org.apache.myfaces.custom.tree2;
 
+
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class TreeNodeBase implements TreeNode
+public class TreeNodeBase implements TreeNode, Comparable
 {
     private static final long serialVersionUID = 278589014441538822L;
     private ArrayList children = new ArrayList();
     private String type;
     private String description;
-    private boolean leaf;    
+    private boolean leaf;
     private String identifier;
 
     public TreeNodeBase()
     {}
-    
+
     public TreeNodeBase(String type, String description, boolean leaf)
     {
         this(type, description, null, leaf);
@@ -46,9 +48,9 @@ public class TreeNodeBase implements TreeNode
 
     public boolean isLeaf()
     {
-        return leaf;
+        return leaf || (getChildCount() == 0);
     }
-    
+
     public void setLeaf(boolean leaf)
     {
         this.leaf = leaf;
@@ -66,31 +68,53 @@ public class TreeNodeBase implements TreeNode
 
     public void setType(String type)
     {
-        this.type = type;           
-    }    
-    
+        this.type = type;
+    }
+
     public void setDescription(String description)
     {
         this.description = description;
     }
-    
+
     public String getDescription()
     {
         return description;
     }
-    
+
     public void setIdentifier(String identifier)
     {
         this.identifier = identifier;
     }
-    
+
     public String getIdentifier()
     {
         return identifier;
     }
-    
+
     public int getChildCount()
     {
         return getChildren().size();
+    }
+
+    public int compareTo(Object obj)
+    {
+        // branches come before leaves, after this criteria nodes are sorted alphabetically
+        TreeNode otherNode = (TreeNode)obj;
+
+        if (isLeaf() && !otherNode.isLeaf())
+        {
+            // leaves come after branches
+            return 1;
+        }
+        else if (!isLeaf() && otherNode.isLeaf())
+        {
+            // branches come before leaves
+            return -1;
+        }
+        else
+        {
+            // both nodes are leaves or both node are branches, so compare the descriptions
+            return getDescription().compareTo(otherNode.getDescription());
+        }
     }
 }
