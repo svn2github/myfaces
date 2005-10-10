5c20d414665ac6efa38e6a9e3d1fb3b428556834
mmarinschek <mmarinschek@13f79535-47bb-0310-9956-ffa450edef68>
2005-10-10 07:45:43 +0000

    moving prototype to tomahawk - needed for InputCalendar
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@312574 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/myfaces/custom/prototype/PrototypeResourceLoader.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2004 by Irian Marinschek & Spiegl Software OEG
+ */
+package org.apache.myfaces.custom.prototype;
+
+/**This class is a mere locator object
+ * for the AddResource class to find the
+ * prototype Resources in the resources package.
+ *
+ * @author Martin Marinschek
+ * @version $Revision: $ $Date: $
+ *          <p/>
+ *          $Log: $
+ */
+public class PrototypeResourceLoader
+{
+    private PrototypeResourceLoader() {}
+}
