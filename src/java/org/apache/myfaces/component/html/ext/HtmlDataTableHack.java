737a070b3c8039e01fc66f14a146e56aee23ca61
mmarinschek <mmarinschek@13f79535-47bb-0310-9956-ffa450edef68>
2005-10-30 12:36:15 +0000

    committed so that this is not lost - will revert to last version immediately
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@329555 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/myfaces/component/html/ext/HtmlDataTableHack.java
+++ src/java/org/apache/myfaces/component/html/ext/HtmlDataTableHack.java
@@ -27,6 +27,7 @@ import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.EditableValueHolder;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIColumn;
 import javax.faces.context.FacesContext;
 import javax.faces.el.ValueBinding;
 import javax.faces.model.ArrayDataModel;
@@ -86,6 +87,112 @@ public abstract class HtmlDataTableHack extends
         return _rowIndex;
     }
 
+    public Object processSaveState(FacesContext context)
+    {
+        Object[] state = new Object[2];
+        state[0] = super.processSaveState(context);
+
+        List childStates = new ArrayList();
+        state[1] = childStates;
+
+        int first = getFirst();
+        int rows = getRows();
+        int last;
+        if (rows == 0)
+        {
+            last = getRowCount();
+        }
+        else
+        {
+            last = first + rows;
+        }
+        for (int rowIndex = first; last==-1 || rowIndex < last; rowIndex++)
+        {
+            setRowIndex(rowIndex);
+
+            //scrolled past the last row
+            if (!isRowAvailable())
+                break;
+
+            for (Iterator it = getChildren().iterator(); it.hasNext();)
+            {
+                UIComponent child = (UIComponent) it.next();
+                if (child instanceof UIColumn)
+                {
+                    if (!child.isRendered())
+                    {
+                        //Column is not visible
+                        continue;
+                    }
+                    for (Iterator columnChildIter = child.getChildren()
+                            .iterator(); columnChildIter.hasNext();)
+                    {
+                        UIComponent columnChild = (UIComponent) columnChildIter
+                                .next();
+                        childStates.add(columnChild.saveState(context));
+                    }
+                }
+            }
+        }
+
+        return state;
+    }
+
+    public void processRestoreState(FacesContext context, Object state)
+    {
+        Object[] values = (Object[]) state;
+
+        setRowIndex(-1);
+
+        super.processRestoreState(context, values[0]);
+
+        List childStates = (List) values[1];
+
+        int first = getFirst();
+        int rows = getRows();
+        int last;
+        if (rows == 0)
+        {
+            last = getRowCount();
+        }
+        else
+        {
+            last = first + rows;
+        }
+        for (int rowIndex = first; last==-1 || rowIndex < last; rowIndex++)
+        {
+            setRowIndex(rowIndex);
+
+            //scrolled past the last row
+            if (!isRowAvailable())
+                break;
+
+            for (Iterator it = getChildren().iterator(); it.hasNext();)
+            {
+                UIComponent child = (UIComponent) it.next();
+                if (child instanceof UIColumn)
+                {
+                    if (!child.isRendered())
+                    {
+                        //Column is not visible
+                        continue;
+                    }
+                    int i=0;
+                    for (Iterator columnChildIter = child.getChildren()
+                            .iterator(); columnChildIter.hasNext();)
+                    {
+                        UIComponent columnChild = (UIComponent) columnChildIter
+                                .next();
+                        columnChild.restoreState(context, childStates.get(i));
+                        i++;
+                    }
+                }
+            }
+        }
+
+        setRowIndex(-1);
+    }
+
     /**
      * @see javax.faces.component.UIData#processUpdates(javax.faces.context.FacesContext)
      */
@@ -117,12 +224,12 @@ public abstract class HtmlDataTableHack extends
      */
     public void encodeBegin(FacesContext context) throws IOException
     {
-        _initialDescendantComponentState = null;
+        //_initialDescendantComponentState = null;
         if (_isValidChilds && !hasErrorMessages(context))
         {
             //Refresh DataModel for rendering:
             _dataModelMap.clear();
-            _rowStates.clear();
+            //_rowStates.clear();
         }
         super.encodeBegin(context);
     }
@@ -265,11 +372,8 @@ public abstract class HtmlDataTableHack extends
                     childState = object[0];
                     descendantState = object[1];
                 }
-                if (component instanceof EditableValueHolder)
-                {
-                    ((EditableValueHolderState) childState)
-                            .restoreState((EditableValueHolder) component);
-                }
+                ((ComponentState) childState)
+                            .restoreState(FacesContext.getCurrentInstance(),component);
                 Iterator childsIterator;
                 if (restoreChildFacets)
                 {
@@ -310,11 +414,7 @@ public abstract class HtmlDataTableHack extends
                 Object descendantState = saveDescendantComponentStates(
                         childsIterator, true);
                 Object state = null;
-                if (child instanceof EditableValueHolder)
-                {
-                    state = new EditableValueHolderState(
-                            (EditableValueHolder) child);
-                }
+                state = new ComponentState(child,FacesContext.getCurrentInstance());
                 childStates.add(new Object[] { state, descendantState });
             }
         }
@@ -450,27 +550,53 @@ public abstract class HtmlDataTableHack extends
         }
     };
 
-    private class EditableValueHolderState
+    private class ComponentState
     {
+        private final boolean _editableValueHolder;
         private final Object _value;
         private final boolean _localValueSet;
         private final boolean _valid;
         private final Object _submittedValue;
+        private final Object _savedState;
 
-        public EditableValueHolderState(EditableValueHolder evh)
+        public ComponentState(UIComponent cmp, FacesContext context)
         {
-            _value = evh.getLocalValue();
-            _localValueSet = evh.isLocalValueSet();
-            _valid = evh.isValid();
-            _submittedValue = evh.getSubmittedValue();
+            _savedState = cmp.saveState(context);
+
+            if(cmp instanceof EditableValueHolder)
+            {
+                EditableValueHolder evh = (EditableValueHolder) cmp;
+
+                _editableValueHolder=true;
+                _value = evh.getLocalValue();
+                _localValueSet = evh.isLocalValueSet();
+                _valid = evh.isValid();
+                _submittedValue = evh.getSubmittedValue();
+            }
+            else
+            {
+                _editableValueHolder=false;
+                _value=null;
+                _localValueSet=false;
+                _valid=false;
+                _submittedValue=null;
+            }
         }
 
-        public void restoreState(EditableValueHolder evh)
+
+        public void restoreState(FacesContext context, UIComponent cmp)
         {
-            evh.setValue(_value);
-            evh.setLocalValueSet(_localValueSet);
-            evh.setValid(_valid);
-            evh.setSubmittedValue(_submittedValue);
+            cmp.restoreState(context,_savedState);
+
+            if(cmp instanceof EditableValueHolder)
+            {
+                EditableValueHolder evh = (EditableValueHolder) cmp;
+
+                evh.setValue(_value);
+                evh.setLocalValueSet(_localValueSet);
+                evh.setValid(_valid);
+                evh.setSubmittedValue(_submittedValue);
+            }
         }
     }
 }
