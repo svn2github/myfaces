e2cad055f1815dd03252d8c9d981c3a5652b2d19
skitching <skitching@13f79535-47bb-0310-9956-ffa450edef68>
2006-01-10 01:09:56 +0000

    Cleaned imports.
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@367446 13f79535-47bb-0310-9956-ffa450edef68

--- tomahawk/src/test/java/org/apache/myfaces/custom/stylesheet/MockResponseWriter.java
+++ tomahawk/src/test/java/org/apache/myfaces/custom/stylesheet/MockResponseWriter.java
@@ -1,12 +1,7 @@
 package org.apache.myfaces.custom.stylesheet;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
-
-import javax.faces.component.UIComponent;
-import javax.faces.context.ResponseWriter;
 
 import org.apache.myfaces.renderkit.html.HtmlResponseWriterImpl;
 
