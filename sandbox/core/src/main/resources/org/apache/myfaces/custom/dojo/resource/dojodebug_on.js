26a470efbfb07ec4b3dbb38bd85d7ac6b01846ce
werpu <werpu@13f79535-47bb-0310-9956-ffa450edef68>
2006-01-27 00:50:53 +0000

    added the debugging flag files
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@372679 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/core/src/main/resources/org/apache/myfaces/custom/dojo/resource/dojodebug_on.js
@@ -0,0 +1,16 @@
+/*
+	Copyright (c) 2004-2005, The Dojo Foundation
+	All Rights Reserved.
+
+	Licensed under the Academic Free License version 2.1 or above OR the
+	modified BSD license. For more information on Dojo licensing, see:
+
+		http://dojotoolkit.org/community/licensing.shtml
+*/
+/*
+* Debug prefix, has to be set 
+* before the dojo include
+*/
+var djConfig = {
+isDebug: true
+};
