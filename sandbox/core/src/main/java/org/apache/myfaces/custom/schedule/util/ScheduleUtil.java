a9ff7aa7a044df0a0fe14805af58ea8fbe326ee0
baranda <baranda@13f79535-47bb-0310-9956-ffa450edef68>
2006-01-29 23:44:52 +0000

    Fixes MYFACES-1037. Thanks to Jurgen Lust
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@373398 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/core/src/main/java/org/apache/myfaces/custom/schedule/util/ScheduleUtil.java
+++ sandbox/core/src/main/java/org/apache/myfaces/custom/schedule/util/ScheduleUtil.java
@@ -380,7 +380,7 @@ public class ScheduleUtil
         if (date == null) return null;
         Calendar cal = GregorianCalendar.getInstance();
         cal.setTime(date);
-        cal.set(Calendar.HOUR, 0);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
