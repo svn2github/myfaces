9e1ebd3d1b08649192846e18c0aab7ce79cb915f
dennisbyrne <dennisbyrne@13f79535-47bb-0310-9956-ffa450edef68>
2006-03-21 05:11:24 +0000

    Two tests for components, tags, renderers, validators, converters, action listeners, phase listeners and core implementation classes in core and tomahawk.
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@387416 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/myfaces/test/TomahawkClassElementTestCase.java
@@ -0,0 +1,17 @@
+package org.apache.myfaces.test;
+
+/**
+ * @author Dennis Byrne
+ */
+
+public class TomahawkClassElementTestCase extends AbstractClassElementTestCase
+{
+
+    public TomahawkClassElementTestCase(){
+        
+        resource.add("META-INF/tomahawk.tld");
+        resource.add("META-INF/faces-config.xml");
+        
+    }
+    
+}
