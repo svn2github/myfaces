da85ff9bd73f28d0e5861238abf0095638d75c44
matzew <matzew@13f79535-47bb-0310-9956-ffa450edef68>
2006-08-01 16:43:48 +0000

    added some test cases
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@427643 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/test/java/org/apache/myfaces/custom/validators/RegExprValidatorTestCase.java
@@ -0,0 +1,82 @@
+package org.apache.myfaces.custom.validators;
+
+import javax.faces.component.UIInput;
+import javax.faces.validator.ValidatorException;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.apache.myfaces.custom.regexprvalidator.RegExprValidator;
+
+public class RegExprValidatorTestCase extends AbstractValidatorTestCase
+{
+
+  public RegExprValidatorTestCase(String arg0) {
+    super(arg0);
+  }
+  
+  RegExprValidator validator;
+  
+  public void setUp()
+  {
+    super.setUp();
+    validator = new RegExprValidator();
+    
+  }
+
+  public void tearDown()
+  {
+    super.tearDown();
+  }
+
+  public static Test suite()
+  {
+    return new TestSuite(RegExprValidatorTestCase.class);
+  }
+  
+  /**
+   * Test when context is set to null
+   */
+  public void testNullContext()
+  {
+
+    doTestNullContext(component, validator);
+  }
+  
+  public void testRightValue()
+  {
+    validator.setPattern("\\d{5}");
+    
+    UIInput comp1 = new UIInput();
+    comp1.setValue("12345");
+    comp1.setId("comp1");
+    facesContext.getViewRoot().getChildren().add(comp1);
+    
+    validator.validate(facesContext, comp1, comp1.getValue());
+
+  }
+
+  public void testWrongValue()
+  {
+    try
+    {
+      validator.setPattern("\\d{12}");
+      
+      UIInput comp1 = new UIInput();
+      comp1.setValue("12345");
+      comp1.setId("comp1");
+      facesContext.getViewRoot().getChildren().add(comp1);
+      
+      validator.validate(facesContext, comp1, comp1.getValue());
+      
+      fail("Expected ValidatorException");
+    }
+    catch (ValidatorException ve)
+    {
+      // if exception then fine.
+    }
+
+  }
+
+  
+}
