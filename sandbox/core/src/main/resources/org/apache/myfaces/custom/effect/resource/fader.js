9f3cb3b17c7fe1cc3cfe1ed59756621ee2d817f3
werpu <werpu@13f79535-47bb-0310-9956-ffa450edef68>
2006-08-21 21:57:39 +0000

    added first prototype replacement class from dojo for the effects tag
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@433380 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/core/src/main/resources/org/apache/myfaces/custom/effect/resource/fader.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+org_apache_myfaces_effects_fader = function (theid, thetime) {
+	fadeid = theid;
+	time = thetime;
+};
+org_apache_myfaces_effects_fader.prototype.hide = function () {
+	dojo.style.setOpacity(dojo.byId(fadeid), 0);
+};
+org_apache_myfaces_effects_fader.prototype.fadeIn = function () {
+	dojo.lfx.html.fadeIn(dojo.byId(fadeid), time).play();
+};
+org_apache_myfaces_effects_fader.prototype.show = function () {
+	dojo.style.setOpacity(dojo.byId(fadeid), 100);
+};
+org_apache_myfaces_effects_fader.prototype.fadeOut = function () {
+	dojo.lfx.html.fadeOut(dojo.byId(fadeid), time).play();
+};
+
