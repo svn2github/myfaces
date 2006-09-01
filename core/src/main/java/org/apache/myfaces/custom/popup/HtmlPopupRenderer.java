b1fb8bbf3cfce986e0e450e75905e2ad7b8ac7bb
matzew <matzew@13f79535-47bb-0310-9956-ffa450edef68>
2006-09-01 04:33:29 +0000

    TOMAHAWK-647. revert Author: matzew Date: Wed Jul 12 22:19:04 2006 New Revision: 421499
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@439175 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/myfaces/custom/popup/HtmlPopupRenderer.java
+++ core/src/main/java/org/apache/myfaces/custom/popup/HtmlPopupRenderer.java
@@ -83,7 +83,7 @@ public class HtmlPopupRenderer
         {
             writer.writeAttribute(HTML.CLASS_ATTR,popup.getStyleClass(),null);
         }
-        renderId(facesContext, popup);
+        writer.writeAttribute(HTML.ID_ATTR, popup.getClientId(facesContext),null);
         writer.writeAttribute(HTML.ONMOUSEOVER_ATTR, new String(popupId+".redisplay();"),null);
 
         Boolean closeExitPopup = popup.getClosePopupOnExitingPopup();
