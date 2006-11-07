56f0efa2992f61318546fb46bd89ef4f143c1cf0
werpu <werpu@13f79535-47bb-0310-9956-ffa450edef68>
2006-11-07 21:14:38 +0000

    added a generic dojo widget interfaces which should encapsule common behavior
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@472269 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/apache/myfaces/custom/dojo/DojoWidget.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.apache.myfaces.custom.dojo;
+
+/**
+ * Basic dojo interface
+ * which has to implement
+ * certain methods regarding the dojo widgets
+ * 
+ * for now the interfaces are very limited and only 
+ * have to implement the variable binding properties
+ * 
+ * 
+ * @author werpu
+ *
+ */
+public interface DojoWidget {
+    /**
+     * getter for an explizit widget
+     * var
+     * @return
+     */
+    public String getWidgetVar();
+    
+    /**
+     * 
+     * @param widgetVar
+     */
+    public void setWidgetVar(String widgetVar);
+    
+}
