f2f1c49635aadb65cab5a77e3beb7f41598cb0ea
grantsmith <grantsmith@13f79535-47bb-0310-9956-ffa450edef68>
2006-11-09 06:34:47 +0000

    MYFACES-1488: License headers (Part 13)
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@472792 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/myfaces/validator/ValidatorBase.java
+++ core/src/main/java/org/apache/myfaces/validator/ValidatorBase.java
@@ -1,30 +1,20 @@
 /*
- * Copyright 2006 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * ValidatorBase provides common attributes and behavior common to all Tomahawk and sandbox validators.
- * Currently this includes:
- *   - getValueBinding(), setValueBinding(), and getFacesContext() methods available in UIComponentBase.
- *   - a message attribute which can be used to override the message key on a per-validator instance.
- *
- * A validator subclass should call getMessage(), and, if not null, use this message key instead.
- * A validator subclass must call super.saveState() and super.restoreState() so that the common attributes are persisted.
- *
- * @author mkienenb (latest modification by $Author$)
- * @version $Revision$
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.myfaces.validator;
 
