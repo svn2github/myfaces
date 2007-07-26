2d51415135f454a732dcee7e80a1d27a9da3615f
mmarinschek <mmarinschek@13f79535-47bb-0310-9956-ffa450edef68>
2007-07-26 09:05:17 +0000

    https://issues.apache.org/jira/browse/TOMAHAWK-1065: fixed missing method
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@559760 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/core/src/main/java/org/apache/myfaces/renderkit/freemarker/DefaultTemplateEncoder.java
+++ sandbox/core/src/main/java/org/apache/myfaces/renderkit/freemarker/DefaultTemplateEncoder.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.myfaces.renderkit.freemarker;
 
 import java.io.IOException;
@@ -22,7 +40,7 @@ public class DefaultTemplateEncoder implements TemplateEncoder {
 
     private static final Log log = LogFactory.getLog(DefaultTemplateEncoder.class);
 
-    public void encodeTemplate(FacesContext context, UIComponent component, String template) throws IOException {    
+    public void encodeTemplate(FacesContext context, UIComponent component, String template, Object dataModel) throws IOException {
             Configuration cfg = new Configuration();
         log.info("Encoding template : " + getClass().getResource(template));
         TemplateLoader templateLoader = new ClassTemplateLoader(getClass(), template);
@@ -31,7 +49,7 @@ public class DefaultTemplateEncoder implements TemplateEncoder {
         Template temp = cfg.getTemplate(template);
         try
         {
-            temp.process(getDatamodel(context, component), context.getResponseWriter());
+            temp.process(dataModel, context.getResponseWriter());
         }
         catch (TemplateException e)
         {
