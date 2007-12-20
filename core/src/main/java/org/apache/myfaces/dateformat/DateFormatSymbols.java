689bd1fbf0f7833a31c59786b8b50a6d9455b457
skitching <skitching@13f79535-47bb-0310-9956-ffa450edef68>
2007-12-20 13:45:36 +0000

    Add constructor that takes a Locale object.
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@605928 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/myfaces/dateformat/DateFormatSymbols.java
+++ core/src/main/java/org/apache/myfaces/dateformat/DateFormatSymbols.java
@@ -1,6 +1,7 @@
 package org.apache.myfaces.dateformat;
 
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * A simple class that contains locale-specific constants used for date
@@ -9,6 +10,11 @@ import java.util.Date;
  * An instance of this can be created, and the symbols modified before
  * passing it to a SimpleDateFormatter. This allows date formatting and
  * parsing to be localised.
+ * <p>
+ * The standard Java DateFormatSymbols class could be used directly by
+ * SimpleDateFormatter, making this class unnecessary. However javascript
+ * does not have an equivalent class built-in, so to keep symmetry between
+ * the java and javascript versions we have one here too.
  */
 public class DateFormatSymbols
 {
@@ -42,13 +48,28 @@ public class DateFormatSymbols
 			null, "long-name", "short-name"
 	};
 
+	// TODO: move these vars out of this "constant" class.
 	Date threshold;
 	Date twoDigitYearStart;
+
 
 	public DateFormatSymbols()
 	{
         threshold = new Date();
         threshold.setYear(threshold.getYear()-80);
         this.twoDigitYearStart = threshold;
+	}
+
+	public DateFormatSymbols(Locale l)
+	{
+		java.text.DateFormatSymbols src = java.text.DateFormatSymbols.getInstance(l);
+		this.eras = src.getEras();
+		this.months = src.getMonths();
+		this.shortMonths = src.getShortMonths();
+		this.weekdays = src.getWeekdays();
+		this.shortWeekdays = src.getShortWeekdays();
+		this.ampms = src.getAmPmStrings();
+
+		// zoneStrings ??
 	}
 }
