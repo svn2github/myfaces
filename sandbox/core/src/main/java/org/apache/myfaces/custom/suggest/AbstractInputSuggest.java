fe753d1e14a142a52d6e83483e75cd199357df05
skitching <skitching@13f79535-47bb-0310-9956-ffa450edef68>
2008-07-15 16:09:46 +0000

    Trivial license text fixes.
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@676950 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/core/src/main/java/org/apache/myfaces/custom/suggest/AbstractInputSuggest.java
+++ sandbox/core/src/main/java/org/apache/myfaces/custom/suggest/AbstractInputSuggest.java
@@ -15,7 +15,7 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
-*/
+ */
 package org.apache.myfaces.custom.suggest;
 
 import javax.faces.component.html.HtmlInputText;
