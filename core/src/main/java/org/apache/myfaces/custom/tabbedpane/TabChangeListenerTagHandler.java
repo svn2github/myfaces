b7263b5a8b15f25071161c495bccaad5faf1ba7f
skitching <skitching@13f79535-47bb-0310-9956-ffa450edef68>
2008-09-25 14:32:36 +0000

    TOMAHAWK-1339 - Make facelets taghandler support EL expressions in "type" attr, and work the same as the jsp tag.
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@698985 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/myfaces/custom/tabbedpane/TabChangeListenerTagHandler.java
+++ core/src/main/java/org/apache/myfaces/custom/tabbedpane/TabChangeListenerTagHandler.java
@@ -33,13 +33,12 @@ import com.sun.facelets.tag.TagConfig;
 import com.sun.facelets.tag.TagHandler;
 
 /**
- * 
+ * Custom Facelets taghandler for the TabChangeListener class.
+ *  
  * @since 1.1.7
- *
  */
 public class TabChangeListenerTagHandler extends TagHandler
 {
-
     private final TagAttribute typeAttr;
 
     public TabChangeListenerTagHandler(TagConfig config)
@@ -51,27 +50,47 @@ public class TabChangeListenerTagHandler extends TagHandler
     public void apply(FaceletContext faceletContext, UIComponent parent)
             throws IOException, FacesException, FaceletException, ELException
     {
+        // only process this tag if the parent has just been created..
         if (parent.getParent() == null)
         {
             if (parent instanceof HtmlPanelTabbedPane)
             {
-                String className;
-                if (!typeAttr.isLiteral())
+                Object listenerRef;
+                if (typeAttr.isLiteral())
                 {
-                    className = typeAttr.getValue();
+                    listenerRef = typeAttr.getValue();
                 }
                 else
                 {
-                    className = typeAttr.getValue(faceletContext);
+                    listenerRef = typeAttr.getObject(faceletContext);
                 }
-                TabChangeListener listener = (TabChangeListener) ClassUtils
-                        .newInstance(className);
-                ((HtmlPanelTabbedPane) parent).addTabChangeListener(listener);
+                
+                if (listenerRef instanceof String)
+                {
+                    String className = (String) listenerRef;
+                    TabChangeListener listener = (TabChangeListener) ClassUtils.newInstance(className);
+                    ((HtmlPanelTabbedPane) parent).addTabChangeListener(listener);
+                }
+                else if (listenerRef instanceof TabChangeListener)
+                {
+                    TabChangeListener listener = (TabChangeListener) listenerRef;
+                    ((HtmlPanelTabbedPane) parent).addTabChangeListener(listener);
+                }
+                else if (listenerRef == null)
+                {
+                    throw new FaceletException("Property 'type' must not be null.");
+                }
+                else
+                {
+                    throw new FaceletException(
+                       "Property 'type' must be either a string (containing a class name) " +
+                       "or a TabChangeListener instance.");
+                }
             }
             else
             {
-                throw new FacesException(
-                        "Component is not HtmlPanelTabbedPane children");
+                throw new FaceletException(
+                        "Component " + parent.getId() + " is not of type HtmlPanelTabbedPane");
             }
         }
     }
