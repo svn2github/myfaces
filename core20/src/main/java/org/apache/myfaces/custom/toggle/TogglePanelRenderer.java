39e52609649a7b038f86d0e7bd2355375441b0a1
lu4242 <lu4242@13f79535-47bb-0310-9956-ffa450edef68>
2010-06-17 01:40:29 +0000

    TOMAHAWK-1474 Behavior support for components that contains event aware properties in jsf 2.0 module
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@955444 13f79535-47bb-0310-9956-ffa450edef68

--- core20/src/main/java/org/apache/myfaces/custom/toggle/TogglePanelRenderer.java
+++ core20/src/main/java/org/apache/myfaces/custom/toggle/TogglePanelRenderer.java
@@ -22,9 +22,12 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.application.ResourceDependency;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.behavior.ClientBehavior;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 
@@ -33,6 +36,8 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.myfaces.shared_tomahawk.renderkit.RendererUtils;
 import org.apache.myfaces.shared_tomahawk.renderkit.html.HTML;
 import org.apache.myfaces.shared_tomahawk.renderkit.html.HtmlGroupRendererBase;
+import org.apache.myfaces.shared_tomahawk.renderkit.html.HtmlRendererUtils;
+import org.apache.myfaces.shared_tomahawk.renderkit.html.util.ResourceUtils;
 
 /**
  * 
@@ -47,16 +52,33 @@ public class TogglePanelRenderer extends HtmlGroupRendererBase {
 
     private static Log log = LogFactory.getLog(TogglePanelRenderer.class);
 
+    private static final String LAYOUT_BLOCK_VALUE = "block";
+
+    @Override
+    public void decode(FacesContext context, UIComponent component)
+    {
+        super.decode(context, component);
+        
+        HtmlRendererUtils.decodeClientBehaviors(context, component);
+    }
+
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
         RendererUtils.checkParamValidity(context, component, TogglePanel.class);
         //addToggleLinkJavascript(context);
         
         TogglePanel togglePanel = (TogglePanel) component;
-        boolean toggleMode = togglePanel.isToggled();
-        toggleVisibility(togglePanel.getChildren(), toggleMode);
-        
         // render the hidden input field
         ResponseWriter writer = context.getResponseWriter();
+        
+        Map<String, List<ClientBehavior>> behaviors = null;
+        behaviors = togglePanel.getClientBehaviors();
+        if (!behaviors.isEmpty())
+        {
+            ResourceUtils.renderDefaultJsfJsInlineIfNecessary(context, writer);
+        }
+        
+        boolean toggleMode = togglePanel.isToggled();
+        toggleVisibility(togglePanel.getChildren(), toggleMode);
 
         String hiddenFieldId = getHiddenFieldId(context, togglePanel);
 
@@ -69,7 +91,55 @@ public class TogglePanelRenderer extends HtmlGroupRendererBase {
 
         writer.endElement(HTML.INPUT_ELEM);
 
-        super.encodeEnd(context, togglePanel);
+        //super.encodeEnd(context, togglePanel);
+        boolean span = false;
+
+        // will be SPAN or DIV, depending on the layout attribute value
+        String layoutElement = HTML.SPAN_ELEM;
+
+        // if layout is 'block', render DIV instead SPAN
+        String layout = togglePanel.getLayout();
+        if (layout != null && layout.equals(LAYOUT_BLOCK_VALUE))
+        {
+            layoutElement = HTML.DIV_ELEM;
+        }
+
+        if (behaviors != null && !behaviors.isEmpty())
+        {
+            //Render element and id to make javascript work
+            span = true;
+            writer.startElement(layoutElement, component);
+            writer.writeAttribute(HTML.ID_ATTR, component.getClientId(context),null);
+            HtmlRendererUtils.renderHTMLAttributes(writer, component, HTML.UNIVERSAL_ATTRIBUTES);
+            HtmlRendererUtils.renderBehaviorizedEventHandlers(context, writer, component, behaviors);
+        }
+        else
+        {
+            //No behaviors, do it as usual
+            if(component.getId()!=null && !component.getId().startsWith(UIViewRoot.UNIQUE_ID_PREFIX))
+            {
+                span = true;
+
+                writer.startElement(layoutElement, component);
+
+                HtmlRendererUtils.writeIdIfNecessary(writer, component, context);
+
+                HtmlRendererUtils.renderHTMLAttributes(writer, component, HTML.COMMON_PASSTROUGH_ATTRIBUTES);
+            }
+            else
+            {
+                span=HtmlRendererUtils.renderHTMLAttributesWithOptionalStartElement(writer,
+                                                                                 component,
+                                                                                 layoutElement,
+                                                                                 HTML.COMMON_PASSTROUGH_ATTRIBUTES);
+            }
+        }
+
+        RendererUtils.renderChildren(context, component);
+        if (span)
+        {
+            writer.endElement(layoutElement);
+        }
     }
 
     private void toggleVisibility(List children, boolean toggleMode) {
