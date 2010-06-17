442663cf4033f172790f2e4d57d2dd3760a7988d
lu4242 <lu4242@13f79535-47bb-0310-9956-ffa450edef68>
2010-06-17 01:56:04 +0000

    TOMAHAWK-1474 Behavior support for components that contains event aware properties in jsf 2.0 module
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@955449 13f79535-47bb-0310-9956-ffa450edef68

--- core20/src/main/java/org/apache/myfaces/custom/swapimage/HtmlSwapImageRenderer.java
+++ core20/src/main/java/org/apache/myfaces/custom/swapimage/HtmlSwapImageRenderer.java
@@ -19,21 +19,27 @@
 package org.apache.myfaces.custom.swapimage;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import javax.faces.application.ResourceDependency;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIGraphic;
+import javax.faces.component.behavior.ClientBehavior;
+import javax.faces.component.behavior.ClientBehaviorHolder;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.myfaces.custom.navigation.HtmlCommandNavigation;
+import org.apache.myfaces.shared_tomahawk.renderkit.ClientBehaviorEvents;
 import org.apache.myfaces.shared_tomahawk.renderkit.JSFAttr;
 import org.apache.myfaces.shared_tomahawk.renderkit.RendererUtils;
 import org.apache.myfaces.shared_tomahawk.renderkit.html.HTML;
 import org.apache.myfaces.shared_tomahawk.renderkit.html.HtmlRenderer;
 import org.apache.myfaces.shared_tomahawk.renderkit.html.HtmlRendererUtils;
+import org.apache.myfaces.shared_tomahawk.renderkit.html.util.ResourceUtils;
 
 /**
  * @JSFRenderer
@@ -50,6 +56,14 @@ public class HtmlSwapImageRenderer
 {
     private static final Log log = LogFactory.getLog(HtmlSwapImageRenderer.class);
 
+    @Override
+    public void decode(FacesContext context, UIComponent component)
+    {
+        super.decode(context, component);
+        
+        HtmlRendererUtils.decodeClientBehaviors(context, component);
+    }
+    
     public void encodeEnd(FacesContext facesContext, UIComponent uiComponent)
             throws IOException
     {
@@ -59,23 +73,24 @@ public class HtmlSwapImageRenderer
 
         //AddResourceFactory.getInstance(facesContext).addJavaScriptAtPosition(facesContext, AddResource.HEADER_BEGIN,
         //        HtmlSwapImage.class, "swapimage.js");
+        
+        HtmlSwapImage swapImage = (HtmlSwapImage) uiComponent;
+        
+        Map<String, List<ClientBehavior>> behaviors = swapImage.getClientBehaviors();
+        if (!behaviors.isEmpty())
+        {
+            ResourceUtils.renderDefaultJsfJsInlineIfNecessary(facesContext, facesContext.getResponseWriter());
+        }
 
         String url;
-        if (uiComponent instanceof HtmlSwapImage)
+        if (uiComponent.getParent() instanceof HtmlCommandNavigation)
         {
-            if (uiComponent.getParent() instanceof HtmlCommandNavigation)
-            {
-                url = ((HtmlCommandNavigation) uiComponent.getParent()).isActive() ?
-                    ((HtmlSwapImage) uiComponent).getActiveImageUrl() : ((HtmlSwapImage)uiComponent).getUrl();
-            }
-            else
-            {
-                url = ((HtmlSwapImage)uiComponent).getUrl();
-            }
+            url = ((HtmlCommandNavigation) uiComponent.getParent()).isActive() ?
+                ((HtmlSwapImage) uiComponent).getActiveImageUrl() : ((HtmlSwapImage)uiComponent).getUrl();
         }
         else
         {
-            url = (String)uiComponent.getAttributes().get(JSFAttr.URL_ATTR);
+            url = ((HtmlSwapImage)uiComponent).getUrl();
         }
 
         if ((url != null) && (url.length() > 0))
@@ -90,20 +105,49 @@ public class HtmlSwapImageRenderer
                                      facesContext.getExternalContext().encodeResourceURL(src),
                                      null);
 
-            if (uiComponent instanceof HtmlSwapImage)
-            {
-                String swapImageUrl = ((HtmlSwapImage) uiComponent).getSwapImageUrl();
-                swapImageUrl = facesContext.getApplication()
-                .getViewHandler().getResourceURL(facesContext, swapImageUrl);
+            String swapImageUrl = ((HtmlSwapImage) uiComponent).getSwapImageUrl();
+            swapImageUrl = facesContext.getApplication()
+            .getViewHandler().getResourceURL(facesContext, swapImageUrl);
 
+            if (behaviors != null && !behaviors.isEmpty())
+            {
+                if (swapImageUrl != null)
+                {
+                    HtmlRendererUtils.renderBehaviorizedAttribute(facesContext, writer, HTML.ONMOUSEOVER_ATTR, uiComponent, 
+                            ClientBehaviorEvents.MOUSEOVER, null, behaviors, HTML.ONMOUSEOVER_ATTR, swapImage.getOnmouseover(),
+                            "SI_MM_swapImage('" + getClientId(facesContext, uiComponent) + "','','" + facesContext.getExternalContext().encodeResourceURL(swapImageUrl) + "',1);");
+                    HtmlRendererUtils.renderBehaviorizedAttribute(facesContext, writer, HTML.ONMOUSEOUT_ATTR, uiComponent, 
+                            ClientBehaviorEvents.MOUSEOUT, null, behaviors, HTML.ONMOUSEOUT_ATTR, swapImage.getOnmouseout(),
+                            "SI_MM_swapImgRestore();");
+                }
+                else
+                {
+                    HtmlRendererUtils.renderBehaviorizedAttribute(facesContext, writer, HTML.ONMOUSEOVER_ATTR, uiComponent, 
+                            ClientBehaviorEvents.MOUSEOVER, null, behaviors, HTML.ONMOUSEOVER_ATTR, swapImage.getOnmouseover(), null);
+                    HtmlRendererUtils.renderBehaviorizedAttribute(facesContext, writer, HTML.ONMOUSEOUT_ATTR, uiComponent, 
+                            ClientBehaviorEvents.MOUSEOUT, null, behaviors, HTML.ONMOUSEOUT_ATTR, swapImage.getOnmouseout(), null);
+                }
+                
+                HtmlRendererUtils.renderBehaviorizedEventHandlersWithoutOnmouseoverAndOnmouseout(facesContext, writer, uiComponent, behaviors);
+
+                HtmlRendererUtils.renderHTMLAttributes(writer, uiComponent, HTML.UNIVERSAL_ATTRIBUTES);
+                HtmlRendererUtils.renderHTMLAttributes(writer, uiComponent, HTML.IMG_ATTRIBUTES);
+            }
+            else
+            {
                 if (swapImageUrl != null)
                 {
                     writer.writeAttribute(HTML.ONMOUSEOVER_ATTR, "SI_MM_swapImage('" + getClientId(facesContext, uiComponent) + "','','" + facesContext.getExternalContext().encodeResourceURL(swapImageUrl) + "',1);", null);
                     writer.writeAttribute(HTML.ONMOUSEOUT_ATTR, "SI_MM_swapImgRestore();", null);
                 }
+                else
+                {
+                    HtmlRendererUtils.renderHTMLAttribute(writer, uiComponent, HTML.ONMOUSEOVER_ATTR, HTML.ONMOUSEOVER_ATTR);
+                    HtmlRendererUtils.renderHTMLAttribute(writer, uiComponent, HTML.ONMOUSEOUT_ATTR, HTML.ONMOUSEOUT_ATTR);
+                }
+                
+                HtmlRendererUtils.renderHTMLAttributes(writer, uiComponent, HTML.IMG_PASSTHROUGH_ATTRIBUTES_WITHOUT_ONMOUSEOVER_AND_ONMOUSEOUT);
             }
-
-            HtmlRendererUtils.renderHTMLAttributes(writer, uiComponent, HTML.IMG_PASSTHROUGH_ATTRIBUTES_WITHOUT_ONMOUSEOVER_AND_ONMOUSEOUT);
 
             writer.endElement(HTML.IMG_ELEM);
         }
