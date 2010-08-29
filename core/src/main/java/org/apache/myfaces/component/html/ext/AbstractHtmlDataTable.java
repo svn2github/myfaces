eabba05f0494764d1fe95f0e45ed1dcdb8053aa5
lu4242 <lu4242@13f79535-47bb-0310-9956-ffa450edef68>
2010-08-29 04:31:12 +0000

    remove unnecessary call to setPreserveDataModel(null)
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@990488 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/myfaces/component/html/ext/AbstractHtmlDataTable.java
+++ core/src/main/java/org/apache/myfaces/component/html/ext/AbstractHtmlDataTable.java
@@ -867,7 +867,7 @@ public abstract class AbstractHtmlDataTable extends HtmlDataTableHack implements
         if (pdm != null)
         {
             setDataModel(pdm);
-            setPreservedDataModel(null);
+            //setPreservedDataModel(null);
         }
 
         return super.getDataModel();
