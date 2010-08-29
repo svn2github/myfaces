82c79f3680d8d358dcab5b81b07ae445becc5748
lu4242 <lu4242@13f79535-47bb-0310-9956-ffa450edef68>
2010-08-29 02:59:59 +0000

    TOMAHAWK-1537 t:datatable updateModelFromPreservedDataModel does not take into account vb.getType could not return the right type (do not call setPreservedDataModel(null) on getDataModel(), because it breaks state saving)
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@990480 13f79535-47bb-0310-9956-ffa450edef68

--- core20/src/main/java/org/apache/myfaces/component/html/ext/AbstractHtmlDataTable.java
+++ core20/src/main/java/org/apache/myfaces/component/html/ext/AbstractHtmlDataTable.java
@@ -1453,7 +1453,7 @@ public abstract class AbstractHtmlDataTable extends HtmlDataTableHack implements
         if (pdm != null)
         {
             setDataModel(pdm);
-            setPreservedDataModel(null);
+            //setPreservedDataModel(null);
         }
 
         return super.getDataModel();
