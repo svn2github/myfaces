ba7b8c6986d66a19ac5c63bd30874f5a6d6a3060
lu4242 <lu4242@13f79535-47bb-0310-9956-ffa450edef68>
2012-10-08 02:26:13 +0000

    TOMAHAWK-1637 Potential XSS security issue in FieldsetRenderer (fix small bug)
    git-svn-id: http://svn.apache.org/repos/asf/myfaces/tomahawk/trunk@1395438 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/main/java/org/apache/myfaces/renderkit/html/ext/HtmlCheckboxRenderer.java
+++ core/src/main/java/org/apache/myfaces/renderkit/html/ext/HtmlCheckboxRenderer.java
@@ -314,7 +314,7 @@ public class HtmlCheckboxRenderer
         if (pageDirectionLayout)
             writer.startElement(HTML.TR_ELEM, selectMany);
         writer.startElement(HTML.TD_ELEM, selectMany);
-        writer.writeText(selectItemGroup.getLabel());
+        writer.writeText(selectItemGroup.getLabel(), null);
         writer.endElement(HTML.TD_ELEM);
         
         if (pageDirectionLayout) {
